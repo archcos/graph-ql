e70c74f0b1d81ad7cec4599e4461ffb5bbe2414e
archcos <arjay.charcos25@gmail.com>
2024-05-28 10:34:05 +0800

    DB update

--- _db.js
+++ _db.js
@@ -1,25 +1,25 @@
-let games = [
-  {id: '1', title: 'Zelda, Tears of the Kingdom', platform: ['Switch']},
-  {id: '2', title: 'Final Fantasy 7 Remake', platform: ['PS5', 'Xbox']},
-  {id: '3', title: 'Elden Ring', platform: ['PS5', 'Xbox', 'PC']},
-  {id: '4', title: 'Mario Kart', platform: ['Switch']},
-  {id: '5', title: 'Pokemon Scarlet', platform: ['PS5', 'Xbox', 'PC']},
+let posts = [
+  {id: '1', title: 'Zelda, Tears of the Kingdom'},
+  {id: '2', title: 'Final Fantasy 7 Remake'},
+  {id: '3', title: 'Elden Ring'},
+  {id: '4', title: 'Mario Kart'},
+  {id: '5', title: 'Pokemon Scarlet'},
 ]
 
-let authors = [
-  {id: '1', name: 'mario', verified: true},
-  {id: '2', name: 'yoshi', verified: false},
-  {id: '3', name: 'peach', verified: true},
+let users = [
+  {id: '1', name: 'mario'},
+  {id: '2', name: 'yoshi'},
+  {id: '3', name: 'peach'},
 ]
 
-let reviews = [
-  {id: '1', rating: 9, content: 'lorem ipsum', author_id: '1', game_id: '2'},
-  {id: '2', rating: 10, content: 'lorem ipsum', author_id: '2', game_id: '1'},
-  {id: '3', rating: 7, content: 'lorem ipsum', author_id: '3', game_id: '3'},
-  {id: '4', rating: 5, content: 'lorem ipsum', author_id: '2', game_id: '4'},
-  {id: '5', rating: 8, content: 'lorem ipsum', author_id: '2', game_id: '5'},
-  {id: '6', rating: 7, content: 'lorem ipsum', author_id: '1', game_id: '2'},
-  {id: '7', rating: 10, content: 'lorem ipsum', author_id: '3', game_id: '1'},
+let comments = [
+  {id: '1', rating: 9, content: 'lorem ipsum', user_id: '1', post_id: '2'},
+  {id: '2', rating: 10, content: 'lorem ipsum', user_id: '2', post_id: '1'},
+  {id: '3', rating: 7, content: 'lorem ipsum', user_id: '3', post_id: '3'},
+  {id: '4', rating: 5, content: 'lorem ipsum', user_id: '2', post_id: '4'},
+  {id: '5', rating: 8, content: 'lorem ipsum', user_id: '2', post_id: '5'},
+  {id: '6', rating: 7, content: 'lorem ipsum', user_id: '1', post_id: '2'},
+  {id: '7', rating: 10, content: 'lorem ipsum', user_id: '3', post_id: '1'},
 ]
 
-export default { games, authors, reviews }
+export default { posts, users, comments }
